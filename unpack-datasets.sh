3352312b4ffb59e4776586587d1cb46178dc523a
micah-lai <micahlai@gmail.com>
2024-08-01 18:01:10 -0700

    download and unpack datasets

---
+++ unpack-datasets.sh
@@ -0,0 +1,111 @@
+name="car-dent"
+url="https://universe.roboflow.com/ds/4KaEBfQEzu?key=n1ZJJuNtkt"
+
+curl -L --output "/lab/micah/obj-det/datasets/$name.zip" $url &gt
+
+wait
+mkdir /lab/micah/obj-det/datasets/$name
+unzip /lab/micah/obj-det/datasets/$name.zip -d /lab/micah/obj-det/datasets/$name
+wait
+rm /lab/micah/obj-det/datasets/$name.zip
+
+name="cows"
+url="https://universe.roboflow.com/ds/GdSQdvReIp?key=Czxbltljm5"
+
+curl -L --output "/lab/micah/obj-det/datasets/$name.zip" $url &gt
+
+wait
+mkdir /lab/micah/obj-det/datasets/$name
+unzip /lab/micah/obj-det/datasets/$name.zip -d /lab/micah/obj-det/datasets/$name
+wait
+rm /lab/micah/obj-det/datasets/$name.zip
+
+name="rock-climbing"
+url="https://universe.roboflow.com/ds/u45A7IsueJ?key=WACBfkONb4"
+
+curl -L --output "/lab/micah/obj-det/datasets/$name.zip" $url &gt
+
+wait
+mkdir /lab/micah/obj-det/datasets/$name
+unzip /lab/micah/obj-det/datasets/$name.zip -d /lab/micah/obj-det/datasets/$name
+wait
+rm /lab/micah/obj-det/datasets/$name.zip
+
+name="clouds"
+url="https://universe.roboflow.com/ds/BHmSrmqigD?key=YlnebbXPMj"
+
+curl -L --output "/lab/micah/obj-det/datasets/$name.zip" $url &gt
+
+wait
+mkdir /lab/micah/obj-det/datasets/$name
+unzip /lab/micah/obj-det/datasets/$name.zip -d /lab/micah/obj-det/datasets/$name
+wait
+rm /lab/micah/obj-det/datasets/$name.zip
+
+name="mri"
+url="https://universe.roboflow.com/ds/JRkbqbX2dk?key=XNY4iLOjVY"
+
+curl -L --output "/lab/micah/obj-det/datasets/$name.zip" $url &gt
+
+wait
+mkdir /lab/micah/obj-det/datasets/$name
+unzip /lab/micah/obj-det/datasets/$name.zip -d /lab/micah/obj-det/datasets/$name
+wait
+rm /lab/micah/obj-det/datasets/$name.zip
+
+name="frc"
+url="https://universe.roboflow.com/ds/CWfFx0ckeI?key=hk1wlv42WN"
+
+curl -L --output "/lab/micah/obj-det/datasets/$name.zip" $url &gt
+
+wait
+mkdir /lab/micah/obj-det/datasets/$name
+unzip /lab/micah/obj-det/datasets/$name.zip -d /lab/micah/obj-det/datasets/$name
+wait
+rm /lab/micah/obj-det/datasets/$name.zip
+
+name="tesla"
+url="https://universe.roboflow.com/ds/NUWABFJoj3?key=3SpVeQU5ib"
+
+curl -L --output "/lab/micah/obj-det/datasets/$name.zip" $url &gt
+
+wait
+mkdir /lab/micah/obj-det/datasets/$name
+unzip /lab/micah/obj-det/datasets/$name.zip -d /lab/micah/obj-det/datasets/$name
+wait
+rm /lab/micah/obj-det/datasets/$name.zip
+
+name="thermal"
+url="https://universe.roboflow.com/ds/5Mvwj8r6Mz?key=6Le5RgucT9"
+
+curl -L --output "/lab/micah/obj-det/datasets/$name.zip" $url &gt
+
+wait
+mkdir /lab/micah/obj-det/datasets/$name
+unzip /lab/micah/obj-det/datasets/$name.zip -d /lab/micah/obj-det/datasets/$name
+wait
+rm /lab/micah/obj-det/datasets/$name.zip
+
+name="aerial-cars"
+url="https://universe.roboflow.com/ds/y2IdPZeVWG?key=bkmigc9DZN"
+
+curl -L --output "/lab/micah/obj-det/datasets/$name.zip" $url &gt
+
+wait
+mkdir /lab/micah/obj-det/datasets/$name
+unzip /lab/micah/obj-det/datasets/$name.zip -d /lab/micah/obj-det/datasets/$name
+wait
+rm /lab/micah/obj-det/datasets/$name.zip
+
+name="parking-lot"
+url="https://universe.roboflow.com/ds/neOp2BMU4o?key=KKer0aSAu5"
+
+curl -L --output "/lab/micah/obj-det/datasets/$name.zip" $url &gt
+
+wait
+mkdir /lab/micah/obj-det/datasets/$name
+unzip /lab/micah/obj-det/datasets/$name.zip -d /lab/micah/obj-det/datasets/$name
+wait
+rm /lab/micah/obj-det/datasets/$name.zip
+
+
