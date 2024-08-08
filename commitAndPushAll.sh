7d7dce11bed14f60568571247b9123268abd3990
micah-lai <micahlai@gmail.com>
2024-08-08 16:12:49 -0700

    test

--- commitAndPushAll.sh
+++ commitAndPushAll.sh
@@ -1,3 +1,3 @@
 git add commitAndPushAll.sh
 git commit -m "$1"
-git push -u https://<$2>:<$3>@<github.com/micahlai/obj-det> main
+git push -u https://<$2>:<$3>@<github.com/micahlai/obj-det.git> main
